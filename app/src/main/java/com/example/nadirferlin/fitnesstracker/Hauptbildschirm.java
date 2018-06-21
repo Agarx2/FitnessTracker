d0383534cce9797b1b518dc9b4008d4fb68d2801
manuel.dutli <manecraft21@gmail.com>
2018-06-21 10:00:59 +0200

    added profile function

--- app/src/main/java/com/example/nadirferlin/fitnesstracker/Hauptbildschirm.java
+++ app/src/main/java/com/example/nadirferlin/fitnesstracker/Hauptbildschirm.java
@@ -1,7 +1,11 @@
 package com.example.nadirferlin.fitnesstracker;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,10 +21,16 @@ public class Hauptbildschirm extends FragmentActivity implements OnMapReadyCallb
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hauptbildschirm);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    public void openActivityProfile(View v){
+        Intent thisIntent = new Intent(this, profilActivity.class);
+        startActivity(thisIntent);
     }
 
 
