d0c4e7ea1bcee0b0c3357ec75245b4f8ecf5abae
nadir.ferlin <nadir-ferlin@hotmail.com>
2018-06-21 10:58:16 +0200

    style Layouts

--- app/src/main/java/com/example/nadirferlin/fitnesstracker/registrierung.java
+++ app/src/main/java/com/example/nadirferlin/fitnesstracker/registrierung.java
@@ -1,8 +1,10 @@
 package com.example.nadirferlin.fitnesstracker;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +16,22 @@ public class registrierung extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrierung);
+
+        final Button btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    btnSignUp.setBackgroundColor(getResources().getColor(R.color.sonicBackgroundShadow));
+                    return true;
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    btnSignUp.setBackgroundColor(getResources().getColor(R.color.sonicBackgroundColor));
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void signUp(View v){
